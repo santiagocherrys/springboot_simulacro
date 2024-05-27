24b2cb5de7d3304127842e10ade330b4bc8eaf0b
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-05-27 11:12:42 -0500

    Se añade las carpetas desde spring initializer

---
+++ src/main/java/com/riwi/springboot_simulacro/SpringbootSimulacroApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.springboot_simulacro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSimulacroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSimulacroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/springboot_simulacro/SpringbootSimulacroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.springboot_simulacro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSimulacroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
